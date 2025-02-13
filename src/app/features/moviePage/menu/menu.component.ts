95097e17b5c67af659a4cfb882884802ab2ea060
Xiomara Andrea <xiomaraa.pulido@gmail.com>
2025-02-13 10:43:28 +0100

    Styles menu

--- src/app/features/moviePage/menu/menu.component.ts
+++ src/app/features/moviePage/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
   host: { class: 'app-sidebar' },
